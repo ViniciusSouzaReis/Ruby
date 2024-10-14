7569f7c49f79b966ec67bb1ec3a68ecd61331c87
Vinicius Reis <viniciusldsr@gmail.com>
2024-10-14 16:22:36 -0300

    wip

---
+++ gets-and-puts/gets-and-puts.rb
@@ -0,0 +1,10 @@
+puts "Hello"
+puts "Enter your name:"
+
+# .chomp removes /n
+# name = gets.chomp
+name = gets
+
+# .inspect inspect what is inside this variable
+puts "Welcome - #{name.inspect}"
+puts "Welcome - #{name}"
